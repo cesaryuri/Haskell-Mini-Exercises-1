908987fd1d6cdfae8b2308589da0cd302c689214
Cesar Yuri e Silva Santos <91036277+cesaryuri@users.noreply.github.com>
2024-09-23 18:48:36 -0300

    Update 4.hs

--- 4.hs
+++ 4.hs
@@ -1,3 +1,4 @@
 --`ax^2 + bx + c` para os parâmetros `a`, `b`, `c` e `x`.?
 calcularfunc :: Float -> Float -> Float -> Float -> Float
 calcularfunc a b c x = a*x^2 + b*x + c
+--tipo da função é de Float's
